1e47fb6b66da70e338049212a07a21edaef22036
Kyle <techgamer@comcast.net>
2015-03-19 15:21:49 -0500

    Added a particle effect for a spell. Got mouse tracking working.

--- CasterMagic/Assets/Scripts/ReticleScript.cs
+++ CasterMagic/Assets/Scripts/ReticleScript.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class ReticleScript : MonoBehaviour 
 {
     public Texture2D reticle;
+    public Rect reticleRect;
 
     public Vector3 lastPos;
+    public Vector3 mousePos;
     public float nativeRatio;
+    
 
 	// Use this for initialization
 	void Start ()
@@ -20,14 +23,21 @@ public class ReticleScript : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-        Vector3 mousePos = Input.mousePosition;
+        mousePos = Input.mousePosition;
 
         if (mousePos != lastPos)
             lastPos = mousePos;
 
-	    guiTexture.texture = reticle;
-
         //transform.Translate(new Vector3(mousePos.x, mousePos.y, 0.0f));
-        transform.position.Set(mousePos.x, mousePos.y, 0.0f);
+        //transform.position.Set(mousePos.x, mousePos.y, 0.0f);
+    }
+
+    void OnGUI()
+    {
+        //guiTexture.texture = reticle;
+        reticleRect = new Rect(mousePos.x - (reticle.width / 2), (Screen.height - mousePos.y) - (reticle.height/2),
+            reticle.width, reticle.height);
+
+        GUI.DrawTexture(reticleRect, reticle);
     }
 }
